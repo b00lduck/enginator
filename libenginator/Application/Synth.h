eed11c1c5a4b7c23b531798ec9d4f6b85ceabb66
daniel <daniel@b3a2f769-a7ff-4d38-93eb-04cb15c46fdf>
2009-04-14 13:54:41 +0000

    git-svn-id: https://www.network-innovation.de/svn/enginator/trunk@21 b3a2f769-a7ff-4d38-93eb-04cb15c46fdf

---
+++ libenginator/Application/Synth.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "vss_stdinc.h"
+#include "Engine.h"
+#include "Exhaust.h"
+#include "Intake.h"
+#include "Limiter.h"
+
+class Synth {
+  
+	public:
+		Synth();
+		~Synth();
+		void synthRender(float* pDataL, float* pDataR, DWORD length_samples);
+
+		Engine *myEngine;
+		Exhaust *myExhaust1;
+		Exhaust *myExhaust2;
+		Intake *myIntake;
+
+		void setThrottle(float throttle);
+		void setRPM(float rpm);
+		void setIgnition(bool ignition);
+		Limiter *myLimiter;
+
+	private:
+
+		float left,right;
+		IIR *myHPL;
+		IIR *myHPR;
+
+};
+
+extern Synth* g_pSynth;
